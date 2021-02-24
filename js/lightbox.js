0779ce1a9dd9c9481fbd2e4002440d89dec7a3fc
smappio <79371007+smappio@users.noreply.github.com>
2021-02-24 09:14:58 +0100

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -49,7 +49,7 @@
     // maxWidth: 800,
     // maxHeight: 600,
     positionFromTop: 50,
-    resizeDuration: 400,
+    resizeDuration: 200,
     showImageNumberLabel: true,
     wrapAround: true,
     disableScrolling: true,
@@ -61,7 +61,7 @@
     If the caption data is user submitted or from some other untrusted source, then set this to true
     to prevent xss and other injection attacks.
      */
-    sanitizeTitle: false
+    sanitizeTitle: true
   };
 
   Lightbox.prototype.option = function(options) {
